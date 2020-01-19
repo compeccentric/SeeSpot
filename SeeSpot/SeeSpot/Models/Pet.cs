ff24b5c10050099a07174250a262c0ea95e8c7a6
JennAnnDag7 <jenniferd7@hotmail.com>
2020-01-19 12:07:56 -0500

    Add Pet Model

---
+++ SeeSpot/SeeSpot/Models/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeeSpot.Models
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Fixed { get; set; }
+        public string Color { get; set; }
+        public string Breed { get; set; }
+        public string Photo { get; set; }
+    }
+}
