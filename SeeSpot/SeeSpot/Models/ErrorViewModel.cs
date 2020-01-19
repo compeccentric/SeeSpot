28b59b414ffdb992afc786cffaff3e63920e2f4c
compeccentric <40215142+compeccentric@users.noreply.github.com>
2020-01-19 11:01:13 -0500

    Initial setup

---
+++ SeeSpot/SeeSpot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SeeSpot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
