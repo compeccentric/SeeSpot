cf14fc6672914b759b9d465564444a3132406e76
JennAnnDag7 <jenniferd7@hotmail.com>
2020-01-29 18:42:26 -0500

    update database

---
+++ SeeSpot/SeeSpot/Migrations/20200129232409_walk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeeSpot.Migrations
+{
+    public partial class walk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Distance = table.Column<int>(nullable: false),
+                    Poop = table.Column<bool>(nullable: false),
+                    Pee = table.Column<bool>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Time = table.Column<DateTime>(nullable: false),
+                    Notes = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Walks");
+        }
+    }
+}
---
+++ SeeSpot/SeeSpot/Migrations/20200129233539_walkretry.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SeeSpot.Migrations
+{
+    public partial class walkretry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
